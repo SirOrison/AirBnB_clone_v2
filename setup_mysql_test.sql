3a453992c6b8b454437992e8632b5f339deef3e8
Christian Ansre Orison <38013255+SirOrison@users.noreply.github.com>
2023-01-06 17:53:02 +0000

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,12 @@
--- Prepares the MySQL server for this project
-
+-- Creates the database hbnb_test_db with specified paramenters
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
+CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+
+GRANT
+SELECT
+	ON performance_schema.* TO 'hbnb_test' @'localhost';
+
 FLUSH PRIVILEGES;
