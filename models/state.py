4124ea3c5dfe2507a6e154f642a005ab7fe23844
Christian Ansre Orison <38013255+SirOrison@users.noreply.github.com>
2023-01-07 04:19:11 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,34 +1,33 @@
 #!/usr/bin/python3
-"""This is the state class"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-import models
-import os
+from os import getenv
+from models.stringtemplates import HBNB_TYPE_STORAGE, DB
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
+    """
+    State class
+    Relationship between Class state to city
     """
     __tablename__ = 'states'
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if (getenv(HBNB_TYPE_STORAGE) == DB):
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
+        name = ''
+
         @property
         def cities(self):
-            """
-            returns the list of City instances with state_id equals
-            to the current State.id
-            """
-            list_city = []
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+            '''Return a list of city instances in filestorage'''
+            from models import storage
+
+            list_cities = []
+            for city in storage.all:
+                list_cities.append(city)
+
+            return list_cities
